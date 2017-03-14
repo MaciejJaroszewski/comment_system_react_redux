b51b7cdb967c7cbb5906a67d85343293b8ba9173
maciej.k.jaroszewski <maciej.k.jaroszewski@gmail.com>
2017-03-14 21:44:07 +0100

    Add actions for adding posts, update and reset input field

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -1,3 +1,19 @@
 import ActionTypes from './types';
+import { createAction } from '../utils';
 
-// actions
+const addNewPost = (post) => createAction(ActionTypes.ADD_POST, { post });
+const updateInputField = (text) => createAction(ActionTypes.UPDATE_INPUT, { text });
+const resetInputField = () => createAction(ActionTypes.RESET_INPUT);
+
+export const updateInput = (text) => {
+  return (dispatch) => {
+    dispatch(updateInputField(text));
+  };
+};
+
+export const addPost = (post) => {
+  return (dispatch) => {
+    dispatch(addNewPost(post));
+    dispatch(resetInputField());
+  };
+};
--- src/app/actions/types.js
+++ src/app/actions/types.js
@@ -1,4 +1,7 @@
 // action types
 export default {
+  ADD_POST: 'ADD_POST',
+  RESET_INPUT: 'RESET_INPUT',
   
+  UPDATE_INPUT: 'UPDATE_INPUT'
 };
