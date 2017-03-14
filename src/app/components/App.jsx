978bb0d1e27d93dd8ceaaad6c0dbcd4de2a28615
maciej.k.jaroszewski <maciej.k.jaroszewski@gmail.com>
2017-03-14 21:43:05 +0100

    Display added posts

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Row, Col, Modal, Button, Input } from 'react-materialize';
+import Post from './Post';
 import ImmutablePropTypes from 'react-immutable-proptypes';
-import { getApp } from '../selectors';
-import { Collapsible, CollapsibleItem } from 'react-materialize';
+import { getPosts, getForm } from '../selectors';
+import { updateInput, addPost } from '../actions';
 
 class App extends React.Component {
+  renderForm() {
+    return (
+      <div style={{marginTop: '30px'}}>
+        <Input label="Post content" name='postContent' value={this.props.form.get('input')} onChange={(e) => this.props.updateInput(e.target.value)}/>
+        <Button modal='close' onClick={() => this.props.addPost(this.props.form.get('input'))}>Send post</Button>
+      </div>
+    );
+  }
+
+  renderPost(posts) {
+    if (posts.size) {
+      return posts.map((post, index) => <Post key={index} content={post.get('content')} date={post.get('date')} />);
+    }
+  }
+
   render(){
     return (
-      <div className="container">
-        <Collapsible popout accordion={true}>
-          <CollapsibleItem header='First' icon='filter_drama'>
-            Lorem ipsum dolor sit amet.
-          </CollapsibleItem>
-          <CollapsibleItem header='Second' icon='place'>
-            Lorem ipsum dolor sit amet.
-          </CollapsibleItem>
-          <CollapsibleItem header='Third' icon='whatshot'>
-            Lorem ipsum dolor sit amet.
-          </CollapsibleItem>
-        </Collapsible>
+      <div className="container" style={{marginTop: '80px'}}>
+        <Row>
+          <Col s={8} offset='s2'>
+            { this.renderPost(this.props.posts) }
+          </Col>
+        </Row>
+        <Modal header='Create new post' bottomSheet
+          trigger={
+            <Button style={{minWidth: '50px', position: 'fixed', top: '20px', right: '45%'}} waves='light'>Create post</Button>
+          }>
+          {this.renderForm()}
+        </Modal>
       </div>
     );
   }
@@ -26,14 +43,22 @@ class App extends React.Component {
 
 function mapStateToProps(state) {
   return {
-    someProp: getApp(state)
+    posts: getPosts(state),
+    form: getForm(state)
   };
 }
 
 App.propTypes = {
-  someProp: ImmutablePropTypes.map
+  posts: ImmutablePropTypes.list,
+  form: ImmutablePropTypes.map,
+
+  addPost: PropTypes.func.isRequired,
+  updateInput: PropTypes.func.isRequired
 };
 
 export default connect(
-  mapStateToProps, {}
+  mapStateToProps, {
+    addPost,
+    updateInput
+  }
 )(App);
