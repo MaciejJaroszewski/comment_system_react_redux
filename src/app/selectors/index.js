99abce9fa82aee08fc7b65d714f10a3ab1437a0a
maciej.k.jaroszewski <maciej.k.jaroszewski@gmail.com>
2017-03-14 21:39:09 +0100

    Add redux selectors

--- src/app/selectors/index.js
+++ src/app/selectors/index.js
@@ -1,1 +1,5 @@
 export const getApp = (state) => state.get('app');
+export const getBlog = (state) => getApp(state).get('blog');
+
+export const getPosts = (state) => getBlog(state).get('posts');
+export const getForm = (state) => getApp(state).get('form');
