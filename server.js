2ad321ca381dacd1c2acdac9f54a97c9ca7237ed
maciej.k.jaroszewski <maciej.k.jaroszewski@gmail.com>
2017-03-14 20:22:36 +0100

    Fix problem with server dist static file

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('./'));
-app.use(express.static('dist'));
+app.use('/scripts', express.static(`${__dirname}/dist/scripts`));
+app.use('/styles', express.static(`${__dirname}/dist/styles`));
 
-app.get(`${__dirname}`, (req, res) => {
+app.get(`/`, (req, res) => {
   res.sendFile(`${__dirname}/dist/index.html`);
 });
 
