23cd030209b41c25fb8ea362f7c9159b38a46e7f
maciej.k.jaroszewski <maciej.k.jaroszewski@gmail.com>
2017-03-16 20:52:28 +0100

    Add voting actions

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -5,6 +5,9 @@ const addNewPost = (post) => createAction(ActionTypes.ADD_POST, { post });
 const updateInputField = (text) => createAction(ActionTypes.UPDATE_INPUT, { text });
 const resetInputField = () => createAction(ActionTypes.RESET_INPUT);
 
+export const incrementCounter = (id) => createAction(ActionTypes.INCREMENT_COUNTER, { id });
+export const decrementCounter = (id) => createAction(ActionTypes.DECREMENT_COUNTER, { id });
+
 export const updateInput = (text) => {
   return (dispatch) => {
     dispatch(updateInputField(text));
